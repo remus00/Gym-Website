b3f4fffab7c55929b8ce535e2e1b6f8844c0fbda
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-10-06 15:48:42 +0200

    Making everything responsive + menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 import "./Header.css";
 
 const Header = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <div className="header">
             <img src={Logo} alt="logo" className="logo" />
-            <ul className="header-menu">
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+            {menuOpen === false && mobile === true ? (
+                <div className="mobile-div" onClick={() => setMenuOpen(true)}>
+                    <img src={Bars} alt="bars" className="mobile-img" />
+                </div>
+            ) : (
+                <ul className="header-menu">
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpen(false)}
+                            to="hero"
+                            span={true}
+                            smooth={true}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpen(false)}
+                            to="programs"
+                            span={true}
+                            smooth={true}
+                        >
+                            Programs
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpen(false)}
+                            to="reasons"
+                            span={true}
+                            smooth={true}
+                        >
+                            Why us
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpen(false)}
+                            to="plans"
+                            span={true}
+                            smooth={true}
+                        >
+                            Plans
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpen(false)}
+                            to="testimonials"
+                            span={true}
+                            smooth={true}
+                        >
+                            Testimonials
+                        </Link>
+                    </li>
+                </ul>
+            )}
         </div>
     );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,13 @@ import Hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
     const transition = { type: "spring", duration: 3 };
+    const mobile = window.innerWidth <= 768 ? true : false;
     return (
-        <div className="hero">
+        <div className="hero" id="hero">
             <div className="blur hero-blur"></div>
             <div className="left-h">
                 <Header />
@@ -18,7 +20,7 @@ const Hero = () => {
                 {/* the beat add */}
                 <div className="the-best-ad">
                     <motion.div
-                        initial={{ left: "238px" }}
+                        initial={{ left: mobile ? "178px" : "238px" }}
                         whileInView={{ left: "8px" }}
                         transition={{ ...transition, type: "tween" }}
                     ></motion.div>
@@ -45,15 +47,36 @@ const Hero = () => {
                 {/* figures */}
                 <div className="figures">
                     <div>
-                        <span>+ 140</span>
+                        <span>
+                            <NumberCounter
+                                end={140}
+                                start={100}
+                                delay="4"
+                                preFix="+"
+                            />
+                        </span>
                         <span>expert coaches</span>
                     </div>
                     <div>
-                        <span>+978</span>
+                        <span>
+                            <NumberCounter
+                                end={978}
+                                start={800}
+                                delay="4"
+                                preFix="+"
+                            />
+                        </span>
                         <span>members joined</span>
                     </div>
                     <div>
-                        <span>+50</span>
+                        <span>
+                            <NumberCounter
+                                end={50}
+                                start={20}
+                                delay="4"
+                                preFix="+"
+                            />
+                        </span>
                         <span>fitness programs</span>
                     </div>
                 </div>
